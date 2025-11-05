66182f5f6f9724d255e375d0c7fcf1d3154bb7af
Radu Stan <164658219+radu04stan@users.noreply.github.com>
2025-11-05 16:13:59 +0200

    first task

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest/PracticalTest01MainActivity.kt
@@ -0,0 +1,47 @@
+package ro.pub.cs.systems.eim.practicaltest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ro.pub.cs.systems.eim.practicaltest.ui.theme.PracticalTESTTheme
+
+class PracticalTest01MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticalTESTTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticalTESTTheme {
+        Greeting("Android")
+    }
+}
